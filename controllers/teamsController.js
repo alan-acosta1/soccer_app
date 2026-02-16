c78cfa80d40c1ef64a3dac765b78fcbe83607843
Alan Acosta <alan1809sa@gmail.com>
2026-02-16 17:19:18 -0500

    Added team stats and head to head endpoints

--- controllers/teamsController.js
+++ controllers/teamsController.js
@@ -12,9 +12,6 @@ exports.getTeamSquad = async(req,res) => {
             return;
         }
         const response = await soccerClient.get('/players/squads',{params:{team:teamId}});
-        console.log('Full response:', JSON.stringify(response.data.response));
-        console.log('Results:', response.data.results);
-        console.log('Errors:', response.data.errors);
         const squads = response.data.response;
         myCache.set(cacheKey,squads,3600);
         res.json(squads);
@@ -44,6 +41,48 @@ exports.getTeamFixtures =async(req,res) => {
     }catch(error){
         console.error("Can not fetch team's fixtures",error.message);
         res.status(500).json({error:"Can not fetch team's fixtures"});
+
+    }
+}
+exports.getTeamStats = async(req,res) => {
+    try{
+        const teamId = req.params.teamId;
+        const league = req.query.league;
+        const season = req.query.season;
+        const cacheKey = `team-stats-${teamId}-${league}-${season}`;
+        const cacheData = myCache.get(cacheKey);
+        if(cacheData){
+            res.json(cacheData);
+            return;
+        }
+        const response = await soccerClient.get('/teams/statistics',{params:{team:teamId, league:league, season:season}});
+        const teamStats = response.data.response;
+        myCache.set(cacheKey,teamStats,3600);
+        res.json(teamStats);
+    }catch(error){
+        console.error('Can not fetch team stats',error.message);
+        res.status(500).json({error:'Can not fetch team stats'})
+
+    }
+}
+exports.getHeadtoHead = async(req,res) => {
+    try{
+        const teamId = req.params.teamId;
+        const opponentId = req.params.opponentId;
+        const cacheKey = `h2h-${teamId}-${opponentId}`;
+        const cacheData = myCache.get(cacheKey);
+        if(cacheData){
+            res.json(cacheData);
+            return;
+        }
+        const response = await soccerClient.get('/fixtures/headtohead',{params:{h2h:`${teamId}-${opponentId}`}});
+        const headtohead = response.data.response;
+        myCache.set(cacheKey,headtohead,3600);
+        res.json(headtohead);
+    }catch(error){
+        console.error('Can not fetch headtohead',error.message);
+        res.status(500).json({error:'Can not fetch headtohead'});
+        
 
     }
 }
--- routes/teams.js
+++ routes/teams.js
@@ -8,5 +8,6 @@ const teamsController = require('../controllers/teamsController');
 //Define routes for teams endpoints
 router.get('/:teamId/fixtures',teamsController.getTeamFixtures);
 router.get('/:teamId/squad', teamsController.getTeamSquad);
-
+router.get('/:teamId/stats',teamsController.getTeamStats);
+router.get('/:teamId/headtohead/:opponentId', teamsController.getHeadtoHead);
 module.exports = router;
